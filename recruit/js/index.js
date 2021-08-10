ec2d29d8ce726214f6cffebb34e0047c48d0301d
bingkun.yan <bingkun.yan@7road.com>
2021-08-10 18:34:47 +0800

    修改

--- recruit/js/index.js
+++ recruit/js/index.js
@@ -19,15 +19,11 @@ $(function() {
 
     $('.selectCountry input[name="country"]').on('change', function(e) {
         country = $('.selectCountry input[name="country"]:checked').val();
-        if (country) {
-            $('.otherCountry').prop('disabled', true)
-        }
         console.log(country)
     })
     $('.otherCountry').on('blur', function() {
-        country = $('.otherCountry').val();
-        if (country != '') {
-            $('.selectCountry input[name="country"]').prop('disabled', true)
+        if (country == 'others') {
+            country = $('.otherCountry').val();
         }
         console.log(country)
     })
@@ -53,15 +49,11 @@ $(function() {
 
     $('.shooter input[name="shooter"]').on('change', function(e) {
         shooter = $('.shooter input[name="shooter"]:checked').val();
-        if (shooter) {
-            $('.otherShooter').prop('disabled', true)
-        }
         console.log(shooter)
     })
     $('.otherShooter').on('blur', function() {
-        shooter = $('.otherShooter').val();
-        if (shooter != '') {
-            $('.shooter input[name="shooter"]').prop('disabled', true)
+        if (shooter == 'others') {
+            shooter = $('.otherShooter').val();
         }
         console.log(shooter)
     })
@@ -91,7 +83,6 @@ $(function() {
         if (!email) {
             $('.email-tip').show()
             $('.submit-tip').show()
-            return;
         }
         if (terms.length < 3) {
             $('.terms-tip').show()
@@ -99,11 +90,18 @@ $(function() {
             return;
         }
 
-        if (genres.length == 0 || !age || !shooter || !steam || !testClient) {
-            console.log(111)
+        if (
+            !age ||
+            !country || country == '' || country == 'others' ||
+            genres.length == 0 ||
+            !shooter || shooter == '' || shooter == 'others' ||
+            !steam ||
+            !testClient
+        ) {
             $('.submit-tip').show()
             return;
         }
+
 
         $('.submit-tip').hide()
 
@@ -111,34 +109,34 @@ $(function() {
 
         // $.ajax()
         setTimeout(function() {
-            window.location.href = "/case/2020/recruit/result.shtml"
+            // window.location.href = "/case/2020/recruit/result.shtml"
         }, 2000)
         
     })
 
-    $('.selectCountry input[name="country"]').click(function() {
-        var $radio = $(this);
-        if ($radio.data('checked')){
-            $radio.prop('checked', false);
-            $radio.data('checked', false);
-            $('.otherCountry').prop('disabled', false)
-            country = ''
-        } else {
-            $radio.prop('checked', true);
-            $radio.data('checked', true);
-        }
-    })
-    $('.shooter input[name="shooter"]').click(function() {
-        var $radio = $(this);
-        if ($radio.data('checked')){
-            $radio.prop('checked', false);
-            $radio.data('checked', false);
-            $('.otherShooter').prop('disabled', false)
-            shooter = ''
-        } else {
-            $radio.prop('checked', true);
-            $radio.data('checked', true);
-        }
-    })
+    // $('.selectCountry input[name="country"]').click(function() {
+    //     var $radio = $(this);
+    //     if ($radio.data('checked')){
+    //         $radio.prop('checked', false);
+    //         $radio.data('checked', false);
+    //         $('.otherCountry').prop('disabled', false)
+    //         country = ''
+    //     } else {
+    //         $radio.prop('checked', true);
+    //         $radio.data('checked', true);
+    //     }
+    // })
+    // $('.shooter input[name="shooter"]').click(function() {
+    //     var $radio = $(this);
+    //     if ($radio.data('checked')){
+    //         $radio.prop('checked', false);
+    //         $radio.data('checked', false);
+    //         $('.otherShooter').prop('disabled', false)
+    //         shooter = ''
+    //     } else {
+    //         $radio.prop('checked', true);
+    //         $radio.data('checked', true);
+    //     }
+    // })
 
 })
